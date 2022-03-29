a6bce8811211a2253cca4229a301e30873187211
Anjan Talatam <tspa2117@gmail.com>
2022-03-29 22:13:21 +0530

    Update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,19 +26,19 @@ function NavBar() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar color='primary' className={classes.toolBar}>
-          <Link to='/' className={`${classes.navLink} ${classes.navLogo} `}>
-            <Button color='inherit'>
+      <AppBar position="static">
+        <Toolbar color="primary" className={classes.toolBar}>
+          <Link to="/" className={`${classes.navLink} ${classes.navLogo} `}>
+            <Button color="inherit">
               <FunctionsRoundedIcon />
-              <Typography variant='h6'>GPA</Typography>
+              <Typography variant="h6">GPA</Typography>
             </Button>
           </Link>
-          <Link to='/cgpa' className={classes.navLink}>
-            <Button color='inherit'> CGPA Calculator</Button>
+          <Link to="/cgpa" className={classes.navLink}>
+            <Button color="inherit"> CGPA Calculator</Button>
           </Link>
-          <Link to='/sgpa' className={classes.navLink}>
-            <Button color='inherit' className={classes.menuButton}>
+          <Link to="/sgpa" className={classes.navLink}>
+            <Button color="inherit" className={classes.menuButton}>
               SGPA Estimator
             </Button>
           </Link>
@@ -47,8 +47,8 @@ function NavBar() {
               Who are we
             </Button>
           </Link> */}
-          <Link to='/support' className={classes.navLink}>
-            <Button color='inherit' className={classes.menuButton}>
+          <Link to="/support" className={classes.navLink}>
+            <Button color="inherit" className={classes.menuButton}>
               Support
               <LiveHelpOutlinedIcon className={classes.headerHelpIcon} />
             </Button>
